4a042ed19aac94677eb253b34b8a84adca2b5dc4
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-06-08 20:01:55 +0200

    add code for second section of tutorial - part 2

--- sqlalchemy/working_with_database_metadata.py
+++ sqlalchemy/working_with_database_metadata.py
@@ -27,7 +27,7 @@ from sqlalchemy import ForeignKey
 
 # defining table with foreign key and not nullable constraints
 adress_table = Table(
-    "adress",
+    "address",
     metadata_obj,
     Column("id", Integer, primary_key=True),
     Column("user_id", ForeignKey("user_account.id"), nullable=False),
@@ -36,3 +36,41 @@ adress_table = Table(
 
 # to create these tables on db we need to emit ddl
 metadata_obj.create_all(engine)
+
+#  
+from sqlalchemy.orm import DeclarativeBase
+class Base(DeclarativeBase):
+    pass
+
+print(Base.metadata)
+print(Base.registry)
+
+# creating db tables using Declarative in ORM
+from typing import List
+from typing import Optional
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+# creating db tables on db by emitting ddl
+Base.metadata.create_all(engine)
